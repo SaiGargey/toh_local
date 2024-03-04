37322cd18428682e18b85c5c7595a1d609968a90
SaiGargey <saigargey007@gmail.com>
2024-03-04 15:16:33 +0530

    atlas

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ app.use(cors({
 }));
 
 // & MongoDB connection
-mongoose.connect("mongodb://localhost:27017/myFirst")
+mongoose.connect("mongodb+srv://g81psnew:G812023PSNew@g81.gevu1bu.mongodb.net/myFirst")
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Error connecting to MongoDB:', err));
 
